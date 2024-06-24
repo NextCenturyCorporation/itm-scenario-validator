3d4f49b7eeec33debf99feaccd1a4d341f30a814
kaitlyn.sharo <kaitlyn.sharo@caci.com>
2024-06-24 09:17:16 -0400

    emit error for character that is used during a scene where it is removed

--- validator.py
+++ validator.py
@@ -931,11 +931,15 @@ class YamlValidator:
                 # check persist character scenes
                 elif s.get('persist_characters', False):
                     loc = l.split('.')
+                    removed_this_scene = s.get('removed_characters', [])
                     val = self.get_value_at_key(loc, data)
                     if val is not None and val not in all_chars:
                         self.logger.log(LogLevel.ERROR, "Key '" + loc[-1] + "' at '" + str('.'.join(loc)) + "' has value '" + str(val) + "', but that character id is never defined within the scenario yaml file.")
                         self.invalid_values += 1
-                    if not any('removed_characters' in el for el in loc) and val is not None and val in removed_chars:
+                    if val in removed_this_scene:
+                        self.logger.log(LogLevel.ERROR, f"Character ID '{val}' appears in '{str('.').join(loc)}', but is removed during this scene, so cannot be used.")
+                        self.invalid_values += 1
+                    elif not any('removed_characters' in el for el in loc) and val is not None and val in removed_chars:
                         self.logger.log(LogLevel.WARN, f"Character ID '{val}' appears in '{str('.').join(loc)}', but is removed at some point during the scenario. Make sure that this character is not removed before this scene.")
                         self.warning_count += 1
                     
